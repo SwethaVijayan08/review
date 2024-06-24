0ce6bf8074a33fd5ab47c055085b026d02ae20ef
swe <swemadhu84@gmail.com>
2024-06-24 14:04:42 +0530

    created Navigation bar and feedback section

---
+++ src/Components/Feedback.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import bg from './bg.jpeg'
+import {TypeAnimation} from 'react-type-animation';
+
+const Feedback = () => {
+  return (
+    <div className='my-7 sm:my-0 max-w-[1200px] h-auto mx-auto flex flex-col-reverse sm:flex-row justify-center align-center' >
+        <div className= 'flex-col my-auto mx-auto'>
+            <div className='my-auto'>
+                  <img className="w-[300px] sm:w-[300px] mx-auto-h-auto rounded-full" src={bg} alt="mybg"/>
+            </div>
+            <h1 className= 'justify-center align-center md:text-5xl sm:text-5xl text-3xl font-bold md:py-6'>
+                <TypeAnimation
+                sequence={[
+                    "Online Course",
+                    1000, 
+                    "Feedback",
+                    1000,
+                    "Review",
+                    1000, 
+                ]}
+                wrapper="span" 
+                speed= {50} 
+                repeat= {Infinity}
+                />
+                </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Feedback
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React , {useState}from "react";
+import { AiOutlineClose,AiOutlineMenu} from "react-icons/ai";
+
+
+const Navbar = () => {
+
+    const[nav, setNav]= useState(false)
+
+    const handleNav=() =>[
+        setNav(!nav)
+    ]
+
+
+    return(
+        <div>
+        <div className="text-gray-400 flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4 text-lg">
+            <h1 className="text-3xl font-bold primary-color ml-4">
+                Online Sessions
+            </h1>
+            
+            <ul className="hidden md:flex">
+            <li className='p-5'>Courses</li>
+            <li className='p-5'>Login</li>   
+            </ul>
+
+            <div onClick={handleNav} className="block md:hidden">
+                {nav ? <AiOutlineClose size={20}/>:<AiOutlineMenu size={30}/>}
+            </div>
+            <div className={nav ?'text-gray-300 fixed h-full left-0 top-0 w[60%] border-r border-r-gray-900 bg-[#202121] ease-in-out duration-500':"fixed left-[-100%]"}>
+             
+                <ul className="flex md:hidden flex-col">
+                    <li className='p-5'>Courses</li>
+                    <li className='p-5'>Login</li>              
+                </ul>
+            </div>
+        </div>
+    </div>
+    ) 
+}
+
+export default Navbar
